dfe9e3bb0b62956421a35563cc3c2a8a69699ac6
Sebastian <123086236+16BitMoose@users.noreply.github.com>
2024-01-31 14:35:53 +0100

    Dieselknight wireframe with orbitcontrols

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,42 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.IcosahedronGeometry( 1, 0);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const objLoader = new OBJLoader();
+        objLoader.load('assets/static/models/DieselKnight.obj', (object) => {
+            object.position.y = -0.4;
+            scene.add(object);
+        });
 
 camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate() {
-	requestAnimationFrame( animate );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+const animate = () => {
+    requestAnimationFrame(animate);
 
-	renderer.render( scene, camera );
-}
+    controls.update();
+
+    renderer.render(scene, camera);
+};
+
+window.addEventListener('resize', () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+
+    camera.aspect = newWidth / newHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(newWidth, newHeight);
+});
 
 animate();
