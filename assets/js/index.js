689f6b65444d31db45000746ef2726f2bd22a1c5
Sebastian <123086236+16BitMoose@users.noreply.github.com>
2024-02-05 10:39:43 +0100

    Website online testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+const audio = document.getElementById("audio")
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const objLoader = new OBJLoader();
-        objLoader.load('assets/static/models/DieselKnight.obj', (object) => {
-            object.position.y = -0.4;
-            scene.add(object);
-        });
-
-camera.position.z = 5;
-const controls = new OrbitControls(camera, renderer.domElement);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
-
-const animate = () => {
-    requestAnimationFrame(animate);
-
-    controls.update();
-
-    renderer.render(scene, camera);
-};
-
-window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-
-    camera.aspect = newWidth / newHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(newWidth, newHeight);
-});
-
-animate();
+function PlaySound(){
+    audio.play();
+}
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: 'assets/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '16BitMoose.github.io'),
-  },
-};
